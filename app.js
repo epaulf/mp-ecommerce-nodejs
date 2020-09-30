386dff15e4137880b80419c7d3c8735b52879bff
epaulf <ericpaulflorese@gmail.com>
2020-09-30 17:06:31 -0500

    Se arregla package json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var port = process.env.PORT || 8080;
+
 var app = express();
-app.set("port", port);
+
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
@@ -18,4 +18,4 @@ app.use(express.static("assets"));
 
 app.use("/assets", express.static(__dirname + "/assets"));
 
-//app.listen(3000);
+app.listen(process.env.PORT || 3000);
