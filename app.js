4debf6431accaafce434ce3afce7f58003c36592
epaulf <ericpaulflorese@gmail.com>
2020-09-30 17:31:40 -0500

    Se agrega checkout

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+  access_token:
+    "APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181",
+});
 var app = express();
 
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: "Mi producto",
+      unit_price: 100,
+      quantity: 1,
+    },
+  ],
+};
+
+mercadopago.preferences
+  .create(preference)
+  .then(function (response) {
+    // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+    global.id = response.body.id;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
