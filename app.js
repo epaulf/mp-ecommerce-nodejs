c28787777bca48be9168b500250df48bf57e4363
epaulf <ericpaulflorese@gmail.com>
2020-10-02 15:22:46 -0500

    se ajusta checkout a redirect

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.get("/failure", function (req, res) {
 });
 
 app.get("/success", function (req, res) {
-  res.render("success");
+  console.log(req.query);
+  payment_method_id = req.query.payment_method_id;
+  external_reference = req.query.external_reference;
+  payment_id = req.query.payment_id;
+  res.render("success", { payment_method_id, external_reference, payment_id });
 });
 
 app.get("/pending", function (req, res) {
@@ -23,6 +27,7 @@ app.get("/pending", function (req, res) {
 });
 
 app.get("/detail", function (req, res) {
+  // console.log(req.query);
   // SDK de Mercado Pago
   const mercadopago = require("mercadopago");
   // Agrega credenciales
@@ -34,20 +39,44 @@ app.get("/detail", function (req, res) {
 
   // Crea un objeto de preferencia
   let preference = {
+    payment_methods: {
+      excluded_payment_methods: [{ id: "amex" }],
+      excluded_payment_types: [{ id: "atm" }],
+      installments: 6,
+    },
     back_urls: {
       success: "https://epaulf-mp-commerce-nodejs.herokuapp.com/success",
       failure: "https://epaulf-mp-commerce-nodejs.herokuapp.com/failure",
       pending: "https://epaulf-mp-commerce-nodejs.herokuapp.com/pending",
     },
+    external_reference: "102020",
+    notification_url:
+      "https://epaulf-mp-commerce-nodejs.herokuapp.com/notifications",
     auto_return: "approved",
-    external_reference: "1234656789102020",
     items: [
       {
+        id: "1234",
         title: req.query.title,
+        descrption: "Dispositivo móvil de Tienda e-commerce",
+        picture_url: req.query.img,
         unit_price: parseInt(req.query.price),
         quantity: parseInt(req.query.unit),
+        external_reference: "ericpaulflorese@gmailcom",
       },
     ],
+    payer: {
+      first_name: "Lalo",
+      last_name: "Landa",
+      address: {
+        zip_code: "039940",
+        street_name: "Insurgentes Sur",
+        street_number: 1602,
+      },
+      phone: {
+        area_code: "52",
+        number: 5549737300,
+      },
+    },
   };
 
   mercadopago.preferences
@@ -65,6 +94,11 @@ app.get("/detail", function (req, res) {
     .catch(function (error) {
       console.log(error);
     });
+});
+
+app.post("/notifications", function (req, res) {
+  console.log(req.query);
+  res.status(200).json("OK");
 });
 
 app.use(express.static("assets"));
